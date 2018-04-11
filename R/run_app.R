51520c3194eab6cf362a51056567eb9b79e677fd
Ryan Wesslen <ryanwesslen@cciwd-437-59imac.dyn.uncc.edu>
2018-04-11 16:37:05 -0400

    new branch

--- R/run_app.R
+++
@@ -1,9 +0,0 @@
-#' @export
-runApp <- function() {
-  appDir <- system.file("app", package = "topicApp")
-  if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `topicApp`.", call. = FALSE)
-  }
-
-  shiny::runApp(appDir, display.mode = "normal")
-}
